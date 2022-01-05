562086280f42f141fa0ec17280df0c60f3d67588
emmy202 <hitimanaemm10@gmail>
2022-01-05 15:45:43 +0200

    Initial commit

---
+++ src/main/java/com/org/serverconfigtwo/ServerConfigTwoApplication.java
@@ -0,0 +1,13 @@
+package com.org.serverconfigtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConfigTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerConfigTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/serverconfigtwo/ServerConfigTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.serverconfigtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
